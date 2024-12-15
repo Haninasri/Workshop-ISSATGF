4e8f13a750160110c8b7041aabe67d7c175870a6
Haninasri <naasri.hani@gmail.com>
2024-12-15 16:20:22 +0100

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore,doc, getDoc ,setDoc  } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCP0ym6HByWXGhC4qqMoEzOlWT74NZos5U",
-    authDomain: "work-shop-issat.firebaseapp.com",
-    databaseURL: "https://work-shop-issat-default-rtdb.firebaseio.com",
-    projectId: "work-shop-issat",
-    storageBucket: "work-shop-issat.firebasestorage.app",
-    messagingSenderId: "101028528145",
-    appId: "1:101028528145:web:e9c9195e1274a58a1171b0"
+    apiKey: "xxxxxxxx",
+    authDomain: "xxxxxxx",
+    databaseURL: "xxxxxxxm",
+    projectId: "xxxxxxt",
+    storageBucket: "xxxxxxx",
+    messagingSenderId: "xxxxxxxx5",
+    appId: "xxxxxxxxxxxx0"
 };
 
 const app = initializeApp(firebaseConfig);
